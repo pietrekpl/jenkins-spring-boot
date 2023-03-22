8e0c3440eff8cf159f497a03136262624a154f7e
p.ludynia <p.ludynia@godeltech.com>
2023-03-22 11:14:41 +0100

    add sample test

--- src/test/java/com/example/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/example/jenkins/JenkinsApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.jenkins;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class JenkinsApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void justTrue() {
+        assertTrue(true);
     }
 
 }
